145c380ca11b0f2a0953caa6be7cac0a06be6189
noormohammad20 <mdsaim204@gmail.com>
2022-08-17 14:45:41 +0600

    create dynamic hook counter

--- src/components/DynamicHooksCounter.js
+++ src/components/DynamicHooksCounter.js
@@ -1,15 +1,15 @@
 import { useSelector, useDispatch } from "react-redux"
-import { increment, decrement } from "../redux/counter/actions"
+import { increment, decrement } from "../redux/dynamicCounter/actions"
 
-function HooksCounter() {
-    const count = useSelector((state) => state.counter.value)
+function DynamicHooksCounter() {
+    const count = useSelector((state) => state.dynamicCounter.value)
     const dispatch = useDispatch()
 
-    const incrementHandler = () => {
-        dispatch(increment())
+    const incrementHandler = (value) => {
+        dispatch(increment(value))
     }
-    const decrementHandler = () => {
-        dispatch(decrement())
+    const decrementHandler = (value) => {
+        dispatch(decrement(value))
     }
 
     return (
@@ -18,16 +18,16 @@ function HooksCounter() {
             <div className="flex space-x-3">
                 <button
                     className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-                    onClick={incrementHandler}>
+                    onClick={() => incrementHandler(5)}>
                     Increment
                 </button>
                 <button
                     className="bg-red-400 text-white px-3 py-2 rounded shadow"
-                    onClick={decrementHandler}>
+                    onClick={() => decrementHandler(2)}>
                     Decrement
                 </button>
             </div>
         </div>
     )
 }
-export default HooksCounter
+export default DynamicHooksCounter
