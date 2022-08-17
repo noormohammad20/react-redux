0eae6eda9fa57998bd837f5680585b6dddda1f14
noormohammad20 <mdsaim204@gmail.com>
2022-08-17 12:50:27 +0600

    create store for

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css'
-import Counter from './components/Counter'
-
-function App() {
+import Counter from "./components/Counter"
+import { Provider } from "react-redux"
+import store from "./redux/store"
+export default function App() {
   return (
-    <div>
-      <Counter />
-    </div>
+    <Provider store={store}>
+      <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+        <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+          Simple Counter Application
+        </h1>
+
+        <div className="max-w-md mx-auto mt-10 space-y-5">
+          <Counter />
+        </div>
+      </div>
+    </Provider>
   )
 }
-
-export default App
