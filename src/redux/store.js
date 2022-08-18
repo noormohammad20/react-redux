e408571eef1633840c849c6b6f19389ff84a9685
noormohammad20 <mdsaim204@gmail.com>
2022-08-18 22:44:45 +0600

    add middleware for counter

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
-import { createStore } from "redux"
+import { createStore, applyMiddleware } from "redux"
 import rootReducer from "./rootReducer"
+import myLogger from "./middleware/myLogger"
 
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(myLogger))
 
 export default store
